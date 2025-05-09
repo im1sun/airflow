f0bc428949858fafc208eec9e9ae96097196f95b
im1sun <emuce@nate.com>
2025-05-10 00:34:48 +0900

    op_args 테스트용 dag 업로드

--- dags/dags_python_template.py
+++ dags/dags_python_template.py
@@ -9,7 +9,7 @@ with DAG(
     dag_id="dags_python_template",
     schedule="30 9 * * *",
     start_date=pendulum.datetime(2025, 5, 1, tz="Asia/Seoul"),
-    catchup=False,
+    catchup=False
 ) as dag:
     
     def python_function1(start_date, end_date, **kwargs):
