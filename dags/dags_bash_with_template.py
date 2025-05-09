16977c8a7b4d3f96722ae7e261f212080eb541a0
im1sun <emuce@nate.com>
2025-05-09 23:17:40 +0900

    op_args 테스트용 dag 업로드

--- dags/dags_bash_with_template.py
+++ dags/dags_bash_with_template.py
@@ -6,19 +6,22 @@ from airflow.operators.bash import BashOperator
 
 with DAG(
     dag_id="dags_bash_with_template",
-    schedule="0 0 * * *",
+    schedule="10 0 * * *",
     start_date=pendulum.datetime(2025, 5, 1, tz="Asia/Seoul"),
     catchup=False,
-    params={"example_key": "example_value"},
 ) as dag:
     bash_t1 = BashOperator(
         task_id="bash_t1",
-        bash_command="echo whoami",
+        bash_command='echo "data_interval_end: {{ data_interval_end }}"'
     )
 
     bash_t2 = BashOperator(
         task_id="bash_t2",
-        bash_command="echo $HOSTNAME",
+        env={
+            'START_DATE':'{{data_interval_start | ds }}',
+            'END_DATE':'{{data_interval_end | ds }}'
+        },
+        bash_command='echo $START_DATE && echo $END_DATE'
     )
 
     bash_t1 >> bash_t2
