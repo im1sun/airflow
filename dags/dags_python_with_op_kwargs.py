c1173eb9360e63d1b4418046fb628bb1548f7f44
im1sun <emuce@nate.com>
2025-05-09 23:01:08 +0900

    op_args 테스트용 dag 업로드

--- dags/dags_python_with_op_kwargs.py
+++ dags/dags_python_with_op_kwargs.py
@@ -15,7 +15,7 @@ with DAG(
     regist2_t1 = PythonOperator(
         task_id='regist2_t1',
         python_callable=regist2,
-        op_args=['hjkim','man', 'kr', 'seoul']
+        op_args=['hjkim','man', 'kr', 'seoul'],
         op_kwargs={'email':'hjkim_sun@naver.com','phone':'010'}
     )
 
