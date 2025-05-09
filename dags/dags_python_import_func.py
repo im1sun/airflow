4949668b72d740ff54985d4ca5e553408c1f5bc0
im1sun <emuce@nate.com>
2025-05-09 22:03:06 +0900

    python operator 추가

--- dags/dags_python_import_func.py
+++ dags/dags_python_import_func.py
@@ -2,23 +2,17 @@ from airflow import DAG
 import datetime
 import pendulum
 from airflow.operators.python import PythonOperator
-import random
+from common.common_func import get_sftp
 
 
 with DAG(
-    dag_id="dags_python_operator",
+    dag_id="dags_python_import_func",
     schedule="0 0 * * *",
     start_date=pendulum.datetime(2025, 5, 1, tz="Asia/Seoul"),
     catchup=False,
 ) as dag:
-    def select_fruit():
-        fruit = ['APPLE', 'BANANA', 'ORANGE', 'AVOCADO']
-        rand_int = random.randint(0,3)
-        print(fruit[rand_int])
     
-    py_t1 = PythonOperator(
-        task_id='py_t1',
-        python_callable=select_fruit
+    task_get_sftp = PythonOperator(
+        task_id='task_get_sftp',
+        python_callable=get_sftp
     )
-
-    py_t1
