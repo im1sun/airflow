49e9e6c1f1cefb720d8ea1ff1661acc540bf5976
im1sun <emuce@nate.com>
2025-05-10 21:20:12 +0900

    op_args 테스트용 dag 업로드

--- dags/dags_python_with_xcom_eg2.py
+++ dags/dags_python_with_xcom_eg2.py
@@ -1,8 +1,6 @@
 from airflow import DAG
-import datetime
 import pendulum
 from airflow.decorators import task
-
 
 with DAG(
     dag_id="dags_python_with_xcom_eg2",
@@ -18,14 +16,17 @@ with DAG(
     @task(task_id='python_xcom_pull_1')
     def xcom_pull_1(**kwargs):
         ti = kwargs['ti']
-        print(ti)
         value1 = ti.xcom_pull(task_ids='python_xcom_push_by_return')
-        print('xcom_pull  메서드로 직접 찾은 리턴 값:' + value1)
+        print('xcom_pull 메서드로 직접 찾은 리턴 값: ' + value1)
     
     @task(task_id='python_xcom_pull_2')
     def xcom_pull_2(status, **kwargs):
-        print('함수 입력값으로 받은 값:' + status)
-    
-    python_xcom_push_by_return = xcom_push_result
-    xcom_pull_2(python_xcom_push_by_return)
-    python_xcom_push_by_return >> xcom_pull_1()
+        print('함수 입력값으로 받은 값: ' + status)
+
+    # 함수 실행 -> DAG에 태스크 등록
+    push_task = xcom_push_result()
+    pull_task_1 = xcom_pull_1()
+    pull_task_2 = xcom_pull_2(push_task)
+
+    # 의존성 정의
+    push_task >> pull_task_1
