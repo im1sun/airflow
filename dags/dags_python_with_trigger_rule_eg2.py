ef6e531ff723d470fe658a9619222eff3b397782
im1sun <emuce@nate.com>
2025-05-11 08:57:12 +0900

    dags_python_with_trigger_rule_eg1,2 업로드

---
+++ dags/dags_python_with_trigger_rule_eg2.py
@@ -0,0 +1,44 @@
+from airflow import DAG
+import datetime
+import pendulum
+from airflow.operators.python import PythonOperator
+from airflow.operators.bash import BashOperator
+from airflow.exception import AirflowException
+
+with DAG(
+    dag_id="dags_python_with_trigger_rule_eg2",
+    schedule=None,
+    start_date=pendulum.datetime(2025, 5, 1, tz="Asia/Seoul"),
+    catchup=False,
+) as dag:
+    @task.branch(task_id='branching')
+    def random_branch():
+        import random
+
+        item_list = ['A', 'B', 'C']
+        selected_item = random.choice(item_list)
+        if selected_item == 'A':
+            return 'task_a'
+        elif selected_item == 'B':
+            return 'task_b'
+        elif selected_item == 'C':
+            return 'task_c'
+    
+    task_a = BashOperator(
+        task_id='task_a',
+        bash_commnad='echo upstream1'
+    )
+
+    @task(task_id='task_b')
+    def task_b():
+        print('정상 처리')
+    
+    @task(task_id='task_c')
+    def task_c():
+        print('정상 처리')
+
+    @task(task_id='task_d', trigger_rule='none_skipped')
+    def task_d():
+        print('정상 처리')
+    
+    random_branch() >> [task_a, task_b(), task_c()] >> task_d()
