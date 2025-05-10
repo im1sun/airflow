742277b91a5ceaa7de83561ba319f3501cdabd20
im1sun <emuce@nate.com>
2025-05-10 21:10:17 +0900

    dags_python_with_macro 업로드

--- dags/dags_python_with_xcom_eg1.py
+++ dags/dags_python_with_xcom_eg1.py
@@ -1,22 +1,36 @@
 from airflow import DAG
 import datetime
 import pendulum
-from airflow.operators.python import PythonOperator
-from common.common_func import regist2
+from airflow.decorators import task
 
 
 with DAG(
-    dag_id="dags_python_with_op_kwargs",
+    dag_id="dags_python_with_xcom_eg1",
     schedule="30 6 * * *",
     start_date=pendulum.datetime(2025, 5, 1, tz="Asia/Seoul"),
     catchup=False,
 ) as dag:
     
-    regist2_t1 = PythonOperator(
-        task_id='regist2_t1',
-        python_callable=regist2,
-        op_args=['hjkim','man', 'kr', 'seoul'],
-        op_kwargs={'email':'hjkim_sun@naver.com','phone':'010'}
-    )
+    @task(task_id='python_xcom_push_task1')
+    def xcom_push1(**kwargs):
+        ti = kwargs['ti']
+        print(ti)
+        ti.xcom_push(key="result1", value="value_1")
+        ti.xcom_push(key="result2", value=[1,2,3])
+    
+    @task(task_id='python_xcom_push_task2')
+    def xcom_push2(**kwargs):
+        ti = kwargs['ti']
+        print(ti)
+        ti.xcom_push(key="result1", value="value_2")
+        ti.xcom_push(key="result2", value=[1,2,3,4])
+    
+    @task(task_id='python_xcom_pull_task')
+        ti = kwargs['ti']
+        print(ti)
+        value1 = ti.xcom_pull(key="result1")
+        value2 = ti.xcom_pull(key="result2", task_ids='python_xcom_push_task1')
+        print(value1)
+        print(value2)
 
-    regist2_t1
+    xcom_push1() >> xcom_push2() >> xcom_pull()
--- dags/dags_python_with_xcom_eg2.py
+++ dags/dags_python_with_xcom_eg2.py
@@ -17,11 +17,15 @@ with DAG(
     
     @task(task_id='python_xcom_pull_1')
     def xcom_pull_1(**kwargs):
-        ti = kwargs['t1']
+        ti = kwargs['ti']
         print(ti)
         value1 = ti.xcom_pull(task_ids='python_xcom_push_by_return')
         print('xcom_pull  메서드로 직접 찾은 리턴 값:' + value1)
     
     @task(task_id='python_xcom_pull_2')
-    def xcom_pull_2(**kwargs):
+    def xcom_pull_2(status, **kwargs):
         print('함수 입력값으로 받은 값:' + status)
+    
+    python_xcom_push_by_return = xcom_push_result
+    xcom_pull_2(python_xcom_push_by_return)
+    python_xcom_push_by_return >> xcom_pull_1
