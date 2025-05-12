37229f2b3d621b977e1d733ade41472ea9005a0d
im1sun <emuce@nate.com>
2025-05-12 23:56:46 +0900

    dags_seoul_api_corona 업로드

--- dags/dags_seoul_api_corona.py
+++ dags/dags_seoul_api_corona.py
@@ -14,9 +14,9 @@ with DAG(
     
     tb_cctv_info = SeoulApiToCsvOperator(
         task_id="tb_cctv_info",
-        dataset_nm = 'TbCctvCountStatus',
+        dataset_nm = 'TbOpendataFixedcctvSP',
         path='/opt/airflow/files/TbCctvCountStatus/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash }}',
-        file_name='TbCctvCountStatus.csv'
+        file_name='TbOpendataFixedcctvSP.csv'
     )
 
     # '''서울시 송파구 CCTV 장소 정보'''
