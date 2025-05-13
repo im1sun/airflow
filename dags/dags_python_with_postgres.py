86f5313de48adc2e3661a685b3dc801a6efd7d11
im1sun <emuce@nate.com>
2025-05-13 23:22:07 +0900

    dags_python_with_postgres 업로드

--- dags/dags_python_with_postgres.py
+++ dags/dags_python_with_postgres.py
@@ -18,14 +18,20 @@ with DAG(
 
         # with closing(psycopg2.connect(host=ip, dbname=dbname, user=user, password=passwd, port=int(port))) as conn:
         with closing(psycopg2.connect(host="172.28.0.3", dbname="emuce", user="emuce", password="emuce", port=5432)) as conn:
+            print("<<<<<<<<<<<<<<<<<<<<<<")
             with closing(conn.cursor()) as cursor:
+
                 dag_id = kwargs.get('ti').dag_id
                 task_id = kwargs.get('ti').task_id
                 run_id = kwargs.get('ti').run_id
+                print("<<<<<<<<<<<<<<<<<<<<<<",dag_id)
+                print("<<<<<<<<<<<<<<<<<<<<<<",task_id)
+                print("<<<<<<<<<<<<<<<<<<<<<<",task_id)
                 msg = 'insert 수행'
                 sql = 'insert into py_opr_drct_insrt values (%s,%s,%s,%s);'
                 cursor.execute(sql,(dag_id,task_id,run_id,msg))
                 conn.commit()
+                print("1111111111111111")
     
     insrt_postgres = PythonOperator(
         task_id='insrt_postgres',
