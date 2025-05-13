93759fb5a5ecf160a57f21baf86c439c4977f176
im1sun <emuce@nate.com>
2025-05-13 22:39:24 +0900

    dags_python_with_postgres 업로드

--- dags/dags_python_with_postgres.py
+++ dags/dags_python_with_postgres.py
@@ -29,7 +29,7 @@ with DAG(
     insrt_postgres = PythonOperator(
         task_id='insrt_postgres',
         python_callable=insrt_postgres,
-        op_args=['172.28.0.8', '5432', 'emuce', 'emuce', 'emuce']
+        op_args=['172.28.0.3', '5432', 'emuce', 'emuce', 'emuce']
     )
 
     insrt_postgres
