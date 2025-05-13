8c01a5ee4df6e182a781d437aca22e61aabd7f67
im1sun <emuce@nate.com>
2025-05-13 23:56:41 +0900

    dags_python_with_postgres 업로드

--- dags/dags_python_with_postgres.py
+++ dags/dags_python_with_postgres.py
@@ -1,42 +1,33 @@
 from airflow import DAG
-import datetime
 import pendulum
 from airflow.operators.python import PythonOperator
-from airflow import DAG
-
 
 with DAG(
-    dag_id="dags_python_with_postgres",
+    dag_id='dags_python_with_postgres',
+    start_date=pendulum.datetime(2023,4,1, tz='Asia/Seoul'),
     schedule=None,
-    start_date=pendulum.datetime(2025, 5, 1, tz="Asia/Seoul"),
     catchup=False
 ) as dag:
+
     
     def insrt_postgres(ip, port, dbname, user, passwd, **kwargs):
         import psycopg2
         from contextlib import closing
 
-        # with closing(psycopg2.connect(host=ip, dbname=dbname, user=user, password=passwd, port=int(port))) as conn:
-        with closing(psycopg2.connect(host="172.28.0.3", dbname="emuce", user="emuce", password="emuce", port=5432)) as conn:
-            print("<<<<<<<<<<<<<<<<<<<<<<")
+        with closing(psycopg2.connect(host=ip, dbname=dbname, user=user, password=passwd, port=int(port))) as conn:
             with closing(conn.cursor()) as cursor:
-
                 dag_id = kwargs.get('ti').dag_id
                 task_id = kwargs.get('ti').task_id
                 run_id = kwargs.get('ti').run_id
-                print("<<<<<<<<<<<<<<<<<<<<<<",dag_id)
-                print("<<<<<<<<<<<<<<<<<<<<<<",task_id)
-                print("<<<<<<<<<<<<<<<<<<<<<<",task_id)
-                msg = 'insert 수행'
+                msg = 'insrt 수행'
                 sql = 'insert into py_opr_drct_insrt values (%s,%s,%s,%s);'
                 cursor.execute(sql,(dag_id,task_id,run_id,msg))
                 conn.commit()
-                print("1111111111111111")
-    
+
     insrt_postgres = PythonOperator(
         task_id='insrt_postgres',
         python_callable=insrt_postgres,
         op_args=['172.28.0.3', '5432', 'emuce', 'emuce', 'emuce']
     )
-
+        
     insrt_postgres
