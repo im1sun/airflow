d469999a5fcbce9d3219785c4ffba1b6f588dccc
im1sun <emuce@nate.com>
2025-10-07 22:18:57 +0900

    get_kakao_token

--- plugins/config/get_kakao_token.py
+++ plugins/config/get_kakao_token.py
@@ -1,9 +1,9 @@
 import requests
 # client_id, authorize_code 노출 주의, 실제 값은 임시로만 넣고 Git에 올라가지 않도록 유의
 
-client_id = '{client_id}'
+client_id = '9dd1ba37254bce5b9c64105da44d12a5'
 redirect_uri = 'https://example.com/oauth'
-authorize_code = '{authorize_code}'
+authorize_code = 'uoCUp8-FE4LrXDGaNpOegkoPX6VcdXyqrGEgeb8RvqGg1XBNzrg2YgAAAAQKDRWbAAABmb63oqN-jFVpBnvzXw'
 
 
 token_url = 'https://kauth.kakao.com/oauth/token'
